de2d6943517e9075ebdfc7adcf94dbfc0f860fa4
drak1ng <rodrigo.misquini@gmail.com>
2022-09-22 21:58:39 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,10 +3,10 @@
 for f in /home/*; do
     if [ -d "$f" ]; then
         # Will not run if no directories are available
-        du -sh $f/mail/cur/*
-        du -sh $f/mail/new/*
-        du -sh $f/mail/.spam/*
-        du -sh $f/logs/*
+        du -sh $f/mail/cur
+        du -sh $f/mail/new
+        du -sh $f/mail/.spam
+        du -sh $f/logs
     fi
 done
 
